6ce971ea67f5b430ff1f12a4b1d0debbf2626f05
Kyle <90972744+YangSync@users.noreply.github.com>
2023-11-19 12:34:34 +0800

    update

--- content.js
+++ content.js
@@ -1,12 +1,43 @@
 (function () {
     'use strict';
+    var t;
+
     function skipAd() {
-        var skipButton = document.querySelector('.ytp-ad-text.ytp-ad-skip-button-text');
-        if (skipButton) {
-            skipButton.click();
-            console.log("Auto_skip");
-            clearInterval(t);
+        try {
+            let fulfilledLayout = document.getElementById('fulfilled-layout');
+            if (fulfilledLayout) {
+                fulfilledLayout.remove();
+                console.log('移除成功');
+            }
+        } catch (error) {
+            ;
+        }
+
+        try {
+            let sb1 = document.querySelector('.ytp-ad-skip-button-modern.ytp-button');
+            if (sb1) {
+                sb1.click();
+                console.log('ytp-ad-skip-button-modern success');
+                //clearInterval(t);
+            } else {
+                ;
+            }
+        } catch (error) {
+            ;
+        }
+
+        try {
+            let sb2 = document.querySelector('.ytp-ad-skip-button');
+            if (sb2) {
+                sb2.click();
+                console.log('ytp-ad-skip-button success');
+                //clearInterval(t);
+            } else {
+                ;
+            }
+        } catch (error) {
+            ;
         }
     }
-    var t = setInterval(skipAd, 1000);
+    t = setInterval(skipAd, 1000);
 })();
